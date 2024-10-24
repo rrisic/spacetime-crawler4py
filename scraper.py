3fde7c658332d0cbb4c9bdd54d5d101f0ea95cc2
Ryder Risic <rrisic@uci.edu>
2024-10-24 15:54:27 -0700

    Initial commit

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 import re
 from urllib.parse import urlparse
-
+from bs4 import BeautifulSoup
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
@@ -15,7 +15,20 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+
+    url_list = []
+    if resp.status != 200: # Error on page, skip page
+        error = resp.error
+        print(error)
+        return list()
+    
+    soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
+    links = [tag['href'] for tag in soup.find_all('a', href=True)]
+    for link in links:
+        if is_valid(link):
+            url_list.append(link)
+    
+    return url_list
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
